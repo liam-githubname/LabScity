f9f3e2dea0be6c9bbfceb425e947fa965e50213b
Matt-Salamone <175448127+Matt-Salamone@users.noreply.github.com>
2026-01-17 09:45:17 -0500

    fix: wrap application in mantine provider

---
+++ src/app/theme.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  fontFamily: 'Konkhmer Sleokchher, sans-serif',
+
+  colors: {
+    navy: [
+      "#E7F0F8",
+      "#CFE1F1",
+      "#9CC3E2",
+      "#6AA4D3",
+      "#3885C4",
+      "#073869", // primary Navy
+      "#06325E",
+      "#042749",
+      "#031C35",
+      "#011120",
+    ],
+  },
+  primaryColor: "navy",
+});
