a703c760d4e10819a152c04fbe280bea5e579970
Rafael Niebles <rniebles@proton.me>
2026-01-20 16:32:45 -0500

    HELP ME

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,38 @@
+import { Avatar, Box, Card, Group, Text } from "@mantine/core";
+
 export default function ProfilePage() {
   return (
     <div>
-      <h1>Profile</h1>
-      <p>This is your profile page.</p>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Box style={{ display: "flex", alignItems: "center", gap: 12 }} mb={12}>
+          <Avatar radius="xl" />
+          <Box>
+            <Text size="xl" fw={600}>
+              Rafael Niebles
+            </Text>
+
+            <Box>
+              <Text span>University of Central Florida</Text>
+              <Text span ml={6} color="gray">
+                Student
+              </Text>
+            </Box>
+
+            <Text>Computer Science</Text>
+          </Box>
+        </Box>
+        <Group mb={12}>
+          <Group>
+            <Text fw={600}>About</Text>
+            <Text size="sm">Hello this is my beautiful account</Text>
+          </Group>
+
+          <Group>
+            <Text fw={600}>About</Text>
+            <Text size="sm">Hello this is my beautiful account</Text>
+          </Group>
+        </Group>
+      </Card>
     </div>
   );
 }
