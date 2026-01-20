41b0185c647f3cf56dce7fb4d90bcac8dd9785a6
Rafael Niebles <rniebles@proton.me>
2026-01-20 15:31:07 -0500

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import "@mantine/core/styles.css"; 
+import "@mantine/core/styles.css";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
-import { theme } from "@/lib/constants/theme"; 
+import type { Metadata } from "next";
+import { theme } from "@/lib/constants/theme";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "LabScity",
   description: "Social Media for Scientists",
 };
@@ -13,14 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider theme={theme}>
-          {children}
-        </MantineProvider>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
