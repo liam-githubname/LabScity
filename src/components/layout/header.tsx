e0ba7ca968ca9e4f4585f9e143ace16cd1f48306
Matt-Salamone <175448127+Matt-Salamone@users.noreply.github.com>
2026-01-22 11:09:31 -0500

    refactor: switch to placholder header for clarity (actual header will be recieved from separate branch)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,43 +1,14 @@
 "use client";
 
-import { Group, Text, Button } from "@mantine/core";
+import { Box } from "@mantine/core";
 import classes from "./header.module.css";
 
 export function Header() {
 	return (
-		<Group
+		<Box
 			component="header"
-			justify="space-between"
-			align="center"
-			px="md"
 			bg="gray.0"
 			className={classes.header}
-		>
-			<Text size="lg" c="navy.8" fw={400}>
-				[Header Placeholder - Logo]
-			</Text>
-
-			<Group gap="xl">
-				<Text size="xl" c="navy.8" fw={400} className={classes.navItem}>
-					Home
-				</Text>
-				<Text size="xl" c="navy.8" fw={400} className={classes.navItem}>
-					Discover
-				</Text>
-				<Text size="xl" c="navy.8" fw={400} className={classes.navItem}>
-					Groups
-				</Text>
-				<Text size="xl" c="navy.8" fw={400} className={classes.navItem}>
-					Messages
-				</Text>
-				<Text size="xl" c="navy.8" fw={400} className={classes.navItem}>
-					Notifications
-				</Text>
-			</Group>
-
-			<Button bg="navy.7" c="gray.0" radius="md">
-				Sign Out
-			</Button>
-		</Group>
+		/>
 	);
 }
