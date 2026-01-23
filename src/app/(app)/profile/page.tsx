05f53f1acecb3b6b4aa9a1e2da32666c45dda8d3
simran-dillip <simaug24@gmail.com>
2026-01-23 12:28:25 -0500

    feat: desktop layout added

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ActionIcon,
   Avatar,
@@ -9,6 +11,9 @@ import {
   Group,
   Image,
   Text,
+  Container,
+  Grid,
+  Stack,
 } from "@mantine/core";
 import {
   IconDots,
@@ -291,49 +296,65 @@ const ViewSelector = () => {
 
 export default function ProfilePage() {
   return (
-    <div>
-      <ProfileWidget
-        profileName="Rafael Niebles"
-        profileInstitution="Univ. of Central Florida"
-        profileRole="Student"
-        profileResearchInterest="Machine Learning"
-        profileAbout="Hello this is my beautiful account"
-        profileSkills={["JavaScript", "More JavaScript", "Even MORE JavaScript!", "More More JAVASCRIPT More!!!", "php...!?"]}
-        profilePicURL="https://ih1.redbubble.net/image.5595885630.8128/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
-        profileHeaderImageURL="https://external-preview.redd.it/r6g38aXSaQWtd1KxwJbQ-Fs5jtSVDxX3wtLHJEdqixw.jpg?width=1080&crop=smart&auto=webp&s=87a2c94cb3e1561e2b6abd467ea68d81b9901720"
-      />
+    <Container size="xl" my="lg">
+      <Grid gutter="lg">
+        <Grid.Col span={{ base: 12, md: 8 }}>
+          <Stack gap="lg">
+            <ProfileWidget
+              profileName="Rafael Niebles"
+              profileInstitution="Univ. of Central Florida"
+              profileRole="Student"
+              profileResearchInterest="Machine Learning"
+              profileAbout="Hello this is my beautiful account"
+              profileSkills={[
+                "JavaScript",
+                "More JavaScript",
+                "Even MORE JavaScript!",
+                "More More JAVASCRIPT More!!!",
+                "php...!?",
+              ]}
+              profilePicURL="https://ih1.redbubble.net/image.5595885630.8128/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+              profileHeaderImageURL="https://external-preview.redd.it/r6g38aXSaQWtd1KxwJbQ-Fs5jtSVDxX3wtLHJEdqixw.jpg?width=1080&crop=smart&auto=webp&s=87a2c94cb3e1561e2b6abd467ea68d81b9901720"
+            />
 
-      <Post
-        posterName="Rafael Niebles"
-        posterResearchInterest="JavaScript Hater"
-        posterProfilePicURL="https://pbs.twimg.com/media/DUzbwUdX4AE5RGO.jpg"
-        attachmentPreviewURL="https://s3-eu-west-1.amazonaws.com/images.linnlive.com/d4cf250f63918acf8e5d11b6bfddb6ba/9250355b-75cf-42d8-957b-6d28c6aa930f.jpg"
-        timestamp={new Date()}
-        postText="I think JavaScript is a crime against humanity and it should be explodonated"
-      />
+            <ViewSelector />
 
-      <ViewSelector />
+            <Post
+              posterName="Rafael Niebles"
+              posterResearchInterest="JavaScript Hater"
+              posterProfilePicURL="https://pbs.twimg.com/media/DUzbwUdX4AE5RGO.jpg"
+              attachmentPreviewURL="https://s3-eu-west-1.amazonaws.com/images.linnlive.com/d4cf250f63918acf8e5d11b6bfddb6ba/9250355b-75cf-42d8-957b-6d28c6aa930f.jpg"
+              timestamp={new Date()}
+              postText="I think JavaScript is a crime against humanity and it should be explodonated"
+            />
+          </Stack>
+        </Grid.Col>
 
-      <ProfileListWidget
-        widgetTitle="Following"
-      />
-      <ProfileListWidget
-        widgetTitle="Friends"
-        profiles={[
-          {
-            key: 0,
-            posterName: "Beethoven",
-            posterResearchInterest: "European Music",
-            posterProfilePicURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsxuN8XD4da9_EVO8m6ZP4aECjlYM8mBkbTg&s"
-          },
-          {
-            key: 1,
-            posterName: "2Pac Shakur",
-            posterResearchInterest: "Rap",
-            posterProfilePicURL: "https://npr.brightspotcdn.com/dims4/default/3ef5a7e/2147483647/strip/true/crop/2814x2110+0+0/resize/880x660!/quality/90/?url=https%3A%2F%2Fmedia.npr.org%2Fassets%2Fimg%2F2013%2F07%2F18%2Frexusa_829701b1-31184c163596e9c44d777e61951e2324e3e49487.jpg"
-          }
-        ]}
-      />
-    </div >
+        <Grid.Col span={{ base: 12, md: 4 }}>
+          <Stack gap="lg">
+            <ProfileListWidget widgetTitle="Friends" />
+            <ProfileListWidget
+              widgetTitle="Following"
+              profiles={[
+                {
+                  key: 0,
+                  posterName: "Beethoven",
+                  posterResearchInterest: "European Music",
+                  posterProfilePicURL:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsxuN8XD4da9_EVO8m6ZP4aECjlYM8mBkbTg&s",
+                },
+                {
+                  key: 1,
+                  posterName: "2Pac Shakur",
+                  posterResearchInterest: "Rap",
+                  posterProfilePicURL:
+                    "https://npr.brightspotcdn.com/dims4/default/3ef5a7e/2147483647/strip/true/crop/2814x2110+0+0/resize/880x660!/quality/90/",
+                },
+              ]}
+            />
+          </Stack>
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 }
